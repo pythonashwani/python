6f03fc6d3101fb08f7b6360cfe9fb02c8454b29d
Saxena, Ashwani <pythonashwani@gmail.com>
2024-12-31 15:05:43 +0530

    update

--- 30_2_aws_ec2.py
+++ 30_2_aws_ec2.py
@@ -6,33 +6,30 @@ ec2 = boto3.resource('ec2')
 
 # Launch an EC2 instance
 instance = ec2.create_instances(
-    ImageId='ami-0c55b159cbfafe1f0', # Amazon Linux 2 LTS
+    ImageId='ami-0ca9fb66e076a6e32', # Amazon Linux 2 AMI (HVM) - Kernel 5.10, SSD Volume Type
     MinCount=1,
     MaxCount=1,
     InstanceType='t2.micro',
-    KeyName='my-ec2-key-pair'
+    KeyName='my-ec2-key-pair',
+    TagSpecifications = [
+        {
+            'ResourceType': 'instance',
+            'Tags': [
+                {
+                    'Key': 'Name', # Name of Instance
+                    'Value': 'PythonDemo'
+                },
+                {
+                    'Key': 'Department',
+                    'Value': 'Technical',
+                },
+                {
+                    'Key': 'Environment',
+                    'Value': 'Test'
+                }
+            ]
+        }
+    ]
 )
 
-# Get the instance ID
-instance_id = instance[0].id
-
-# Wait for the instance to be running
-instance[0].wait_until_running()
-
-# Get the instance object
-instance = ec2.Instance(instance_id)
-
-# Run a command on the instance
-response = instance.execute_command(
-    DocumentName='AWS-RunShellScript',
-    Parameters={'commands': ['echo "Hello from EC2!"']}
-)
-
-# Get the output from the command
-output = response['StandardOutputContent']
-
-# Print the output
-print(output)
-
-# Terminate the instance
-instance.terminate()
+print("Instance has been created")
